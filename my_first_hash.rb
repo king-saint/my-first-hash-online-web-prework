dbe14a95da77cd96c9d759956f13c75b016b3a01
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 00:41:27 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,11 +4,7 @@ def my_hash
 end
 
 def shipping_manifest
-   the_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2
-    "oil paintings" => 3
-     }
+   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
      the_manifest
   
 end
@@ -26,10 +22,7 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+                  "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
       shipping_manifest["muskets"] = 2
       shipping_manifest["gun powder"] = 4
